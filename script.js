b8228a867bf4efb50f27447687d45b8668bce0b8
Sunny Patel <sunnypatel2000@yahoo.com>
2020-06-05 13:34:39 -0500

    Wrote functions for add, subtract, multiply, and divide. Set up HTML structure and first div container element.

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.getElementById("container");
+for (let i = 0; i < 16; i++) {
+    
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+
+}
