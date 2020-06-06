568af15a93c9e456adfacf8c61140f5a22cc7122
Sunny Patel <sunnypatel2000@yahoo.com>
2020-06-06 16:47:02 -0500

    Started creating functions for when buttons on the calculator are clicked (not yet functional).

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 const container = document.getElementById("container");
+const addButton = document.getElementById("add").addEventListener("click", addOperator);
+const subtractButton = document.getElementById("subtract").addEventListener("click", subtractOperator);
+const multiplyButton = document.getElementById("multiply").addEventListener("click", multiplyOperator);
+const divideButton = document.getElementById("divide").addEventListener("click", divideOperator);
+const equalButton = document.getElementById("equals").addEventListener("click", operate);
+let operator = "";
+
 for (let i = 0; i <= 9; i++) {
     let digit = document.createElement("div");
     let digitButton = document.createElement("button");
     digitButton.classList.add("digit-" + i);
     digitButton.innerHTML = i;
+    digitButton.addEventListener("click", displayNums);
     digit.appendChild(digitButton);
     container.appendChild(digit);
+}
+
+function addOperator(num1, num2) {
+    operator = "add";
+    return operator;
+}
+
+function subtractOperator(num1, num2) {
+    operator = "subtract";
+    return operator;
+}
+
+function multiplyOperator(num1, num2) {
+    operator = "multiply";
+    return operator;
+}
+
+function divideOperator(num1, num2) {
+    operator = "divide";
+    return operator;
 }
 
 function add(num1, num2) {
@@ -21,9 +49,25 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    if (num2 === 0) {
+        return "Error!";
+    } else {
+        return num1 / num2;
+    }
 }
 
 function operate(operator, num1, num2) {
+    if (operator === "add") {
+        add(num1, num2);
+    } else if (operator === "subtract") {
+        subtract(num1, num2);
+    } else if (operator === "multiply") {
+        multiply(num1, num2);
+    } else {
+        divide(num1, num2);
+    }
+}
 
+function displayNums() {
+    
 }
